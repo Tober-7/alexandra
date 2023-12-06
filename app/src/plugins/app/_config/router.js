612e07fa4fb553166033e78342403e0668818d64
Tobiáš Fašanok <tobi.fasanok@gmail.com>
2023-12-06 15:20:28 +0100

    fixed router

--- app/src/plugins/app/_config/router.js
+++ app/src/plugins/app/_config/router.js
@@ -32,7 +32,6 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-	console.log(from, to);
 	if (to.meta.requiresAuth === true) {
 		if (!Helpers.isAuth()) next({ name: "Auth" });
 		else next();
